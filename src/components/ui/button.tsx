06bd77a4bbeff1ed49cfec6bd91cab0ca1b03d51
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-04-20 13:16:54 +0100

    Create a hover effect for default variant of Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,12 +14,14 @@ const Button = forwardRef<HTMLButtonElement, Props>(
         {...props}
         ref={ref}
         className={cn(
-          "bg-primary text-primary-foreground hover:bg-primary/80 inline-flex h-10 w-fit cursor-pointer items-center justify-center px-8 py-2 text-lg whitespace-nowrap transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
+          "bg-primary group text-primary-foreground relative inline-flex h-10 w-fit cursor-pointer items-center justify-center overflow-hidden px-8 py-2 text-lg whitespace-nowrap transition-colors focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50",
           variant === "outline" &&
             "hover:text-primary border-2 border-white bg-transparent hover:bg-white",
+          variant === "default" && "transition-all duration-300 ease-out",
           className,
         )}
       >
+        <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
         {children}
       </button>
     );
