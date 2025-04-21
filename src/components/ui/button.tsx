960e979f9aab5343a4ae9ddd33ac8459f0ca0a51
EL-OUARDY <ouadie.elouardy@gmail.com>
2025-04-21 13:11:30 +0100

    Create a motion version of the Button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import { motion } from "motion/react";
 import { ButtonHTMLAttributes, ReactElement, forwardRef } from "react";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -21,11 +22,16 @@ const Button = forwardRef<HTMLButtonElement, Props>(
           className,
         )}
       >
-        <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
+        {variant === "default" && (
+          <span className="ease absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12 transform bg-white opacity-10 transition-all duration-1000 group-hover:-translate-x-40"></span>
+        )}
         {children}
       </button>
     );
   },
 );
+
+// Create a motion version of the Button component
+export const MotionButton = motion(Button);
 
 export default Button;
